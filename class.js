03467aaa9f97b9d61f3ef5ba0d9b432c72677f55
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 17:30:16 +0400

    Added Destroyer and Grenade classes

--- class.js
+++ class.js
@@ -20,7 +20,7 @@ class Grass {
 
     mult() {
         this.multiplay++;
-        if (this.multiplay < 1) return;
+        if (this.multiplay < 2) return;
         let emptyCells = this.chooseCell(0);
         if (emptyCells.length > 0) {
             let randIndex = Math.round(Math.random() * (emptyCells.length - 1));
@@ -106,7 +106,7 @@ class GrassEater {
         let y = emptyCells[randIndex][1];
         matrix[this.y][this.x] = 2;
         new GrassEater(x, y);
-        this.energy = 10;
+        this.energy = 5;
     }
 
     eat() {
@@ -120,11 +120,11 @@ class GrassEater {
         }
         matrix[y][x] = 2;
         matrix[this.y][this.x] = 0;
-        this.energy += 2;
+        this.energy += 3;
         this.x = x;
         this.y = y;
     }
-    
+
     move() {
         let emptyCells = this.chooseCell(0);
         let randIndex = Math.round(Math.random() * (emptyCells.length - 1));
@@ -134,7 +134,7 @@ class GrassEater {
         matrix[this.y][this.x] = 0;
         this.x = x;
         this.y = y;
-        this.energy-=2;
+        this.energy -= 2;
     }
 
     remove() {
@@ -149,14 +149,14 @@ class GrassEater {
 
 }
 
-class Monster {
+class Predator {
 
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.directions = [];
-        this.energy = 10;
-        monsters.push(this);
+        this.energy = 20;
+        predators.push(this);
         matrix[y][x] = 3;
     }
 
@@ -191,7 +191,10 @@ class Monster {
             this.eat()
         }
         else if (this.chooseCell(0).length > 0) {
-            this.move()
+            this.move();
+        }
+        else {
+            this.energy -= 0.2;
         }
         if (this.energy >= 20) {
             this.mult()
@@ -210,7 +213,7 @@ class Monster {
         matrix[this.y][this.x] = 0;
         this.x = x;
         this.y = y;
-        this.energy--;
+        this.energy -= 1;
     }
 
     eat() {
@@ -224,7 +227,7 @@ class Monster {
         }
         matrix[y][x] = 3;
         matrix[this.y][this.x] = 0;
-        this.energy += 1;
+        this.energy += 2;
         this.x = x;
         this.y = y;
     }
@@ -236,17 +239,184 @@ class Monster {
         let x = emptyCells[randIndex][0];
         let y = emptyCells[randIndex][1];
         matrix[this.y][this.x] = 3;
-        new Monster(x, y);
-        this.energy = 8;
+        new Predator(x, y);
+        this.energy -= 5;
     }
 
     remove() {
         if (this.energy > 0) return;
         matrix[this.y][this.x] = 0;
-        for (var i in monsters) {
-            if (!(this.x == monsters[i].x && this.y == monsters[i].y)) continue;
-            monsters.splice(i, 1);
+        for (var i in predators) {
+            if (!(this.x == predators[i].x && this.y == predators[i].y)) continue;
+            predators.splice(i, 1);
             break;
+        }
+    }
+
+}
+
+class Destroyer {
+
+    constructor(y) {
+        this.y = y;
+        this.directions = [];
+        for (let j = -2; j <= 2; j++) {
+            let y = this.y + j;
+            for (let x = 0; x < matrix[this.y].length; x++) {
+                if (!(x >= 0 && y >= 0 && x < matrix.length && y < matrix.length)) continue;
+                this.directions.push([x, y]);
+            }
+        }
+        this.cooldown = 20;
+        this.time = 5;
+        destroyers.push(this);
+    }
+
+    start() {
+        this.cooldown--;
+        if (this.cooldown <= 0) {
+            this.destroy();
+        }
+        if (this.time <= 0) {
+            this.mult();
+            this.remove();
+        }
+    }
+
+    destroy() {
+        for (const d in this.directions) {
+            let x = this.directions[d][0];
+            let y = this.directions[d][1];
+            if (!(x >= 0 && y >= 0 && x < matrix.length && y < matrix.length)) continue;
+            this.removeObject(x, y);
+            matrix[y][x] = 4;
+        }
+        this.time--;
+    }
+
+    remove() {
+        for (const d in this.directions) {
+            matrix[this.directions[d][1]][this.directions[d][0]] = 0;
+        }
+        for (var i in destroyers) {
+            if (!(this.x == destroyers[i].x && this.y == destroyers[i].y)) continue;
+            destroyers.splice(i, 1);
+            break;
+        }
+        this.time = 5;
+    }
+
+    mult() {
+        new Destroyer(Math.round(Math.random() * (matrix.length - 1)));
+        this.cooldown = 20;
+    }
+
+    removeObject(x, y) {
+        for (const i in grasses) {
+            if (!(grasses[i].x == x && grasses[i].y == y)) continue;
+            grasses.splice(i, 1);
+        }
+        for (const i in grassEaters) {
+            if (!(grassEaters[i].x == x && grassEaters[i].y == y)) continue;
+            grassEaters.splice(i, 1);
+        }
+        for (const i in predators) {
+            if (!(predators[i].x == x && predators[i].y == y)) continue;
+            predators.splice(i, 1);
+        }
+    }
+
+}
+
+class Grenade {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.bursted = false;
+        this.multiplayCooldown = 10;
+        this.cooldown = 10;
+        this.disappearCooldown = 5;
+        matrix[y][x] = 5;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+            [this.x - 2, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2]
+        ]
+        grenades.push(this);
+    }
+
+    start() {
+        this.cooldown--;
+        this.multiplayCooldown--;
+        if (this.bursted) this.disappearCooldown--;
+        if (this.multiplayCooldown <= 0) {
+            this.multiplayCooldown = 10;
+            this.mult();
+        }
+        if (this.cooldown <= 0) {
+            this.burst();
+        }
+        if (this.disappearCooldown <= 0) {
+            this.remove();
+        }
+    }
+
+    mult() {
+        let x = Math.round(Math.random() * (matrix.length - 1));
+        let y = Math.round(Math.random() * (matrix.length - 1));
+        if (matrix[y][x] == 0) new Grenade(x, y);
+        else this.mult();
+    }
+
+    remove() {
+        for (const d in this.directions) {
+            let x = this.directions[d][0];
+            let y = this.directions[d][1];
+            if (!(x >= 0 && y >= 0 && x < matrix.length && y < matrix.length)) continue;
+            matrix[y][x] = 0;
+        }
+        matrix[this.y][this.x] = 0;
+        for (var i in grenades) {
+            if (!(this.x == grenades[i].x && this.y == grenades[i].y)) continue;
+            grenades.splice(i, 1);
+            break;
+        }
+    }
+
+    burst() {
+        matrix[this.y][this.x] = 5;
+        for (const i in this.directions) {
+            let x = this.directions[i][0];
+            let y = this.directions[i][1];
+            if (!(x >= 0 && y >= 0 && x < matrix.length && y < matrix.length)) continue;
+            matrix[y][x] = 5;
+            this.removeObject(x, y)
+        }
+        this.bursted = true;
+    }
+
+    removeObject(x, y) {
+        for (const i in grassEaters) {
+            if (!(grassEaters[i].x == x && grassEaters[i].y == y)) continue;
+            grassEaters.splice(i, 1);
+        }
+        for (const i in predators) {
+            if (!(predators[i].x == x && predators[i].y == y)) continue;
+            predators.splice(i, 1);
         }
     }
 
--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 matrix = [];
 grasses = [];
 grassEaters = [];
-monsters = [];
+predators = [];
+destroyers = [];
+grenades = [];
 
-var side = 15;
+var side = 10;
 
-function matrixGenerator(size, grassesCount, grassEatersCount, monstersCount) {
+function matrixGenerator(size, grassesAmount, grassEatersAmount, predatorsAmount, destroyersAmount, grenadesAmount) {
     for (let i = 0; i < size; i++) {
         matrix[i] = [];
         for (let j = 0; j < size; j++) matrix[i].push(0);
     }
-    for (let i = 0; i < grassesCount; i++) {
+    for (let i = 0; i < grassesAmount; i++) {
         let x = Math.round(Math.random() * (size - 1));
         let y = Math.round(Math.random() * (size - 1));
         if (matrix[y][x] == 0) new Grass(x, y);
         else i--;
     }
-    for (let i = 0; i < grassEatersCount; i++) {
+    for (let i = 0; i < grassEatersAmount; i++) {
         let x = Math.round(Math.random() * (size - 1));
         let y = Math.round(Math.random() * (size - 1));
         if (matrix[y][x] == 0) new GrassEater(x, y);
         else i--;
     }
-    for (let i = 0; i < monstersCount; i++) {
+    for (let i = 0; i < predatorsAmount; i++) {
         let x = Math.round(Math.random() * (size - 1));
         let y = Math.round(Math.random() * (size - 1));
-        if (matrix[y][x] == 0) new Monster(x, y);
+        if (matrix[y][x] == 0) new Predator(x, y);
+        else i--;
+    }
+    for (let i = 0; i < destroyersAmount; i++) {
+        let x = Math.round(Math.random() * (size - 1));
+        let y = Math.round(Math.random() * (size - 1));
+        if (matrix[y][x] == 0) new Destroyer(Math.round(Math.random() * (matrix.length - 1)));
+        else i--;
+    }
+    for (let i = 0; i < grenadesAmount; i++) {
+        let x = Math.round(Math.random() * (size - 1));
+        let y = Math.round(Math.random() * (size - 1));
+        if (matrix[y][x] == 0) new Grenade(x, y);
         else i--;
     }
 }
 
-matrixGenerator(50, 10, 15, 25);
+matrixGenerator(60, 100, 30, 20, 1, 5);
 
 function setup() {
     createCanvas(matrix.length * side + 1, matrix.length * side + 1);
-    frameRate(60);
     background("#acacac");
 }
 
@@ -46,12 +59,14 @@ function draw() {
             else if (matrix[y][x] == 1) fill("#0ac900");
             else if (matrix[y][x] == 2) fill("#e1ff00");
             else if (matrix[y][x] == 3) fill("#ff0066");
-            else if (matrix[y][x] == 4) fill("#00aeff");
-            else if (matrix[y][x] == 5) fill("#ff9d00");
+            else if (matrix[y][x] == 4) fill("#0341fc");
+            else if (matrix[y][x] == 5) fill("#fc7703");
             rect(x * side, y * side, side, side);
         }
     }
     for (const i in grasses) grasses[i].mult();
     for (const i in grassEaters) grassEaters[i].start();
-    for (const i in monsters) monsters[i].start();
+    for (const i in predators) predators[i].start();
+    for (const i in destroyers) destroyers[i].start();
+    for (const i in grenades) grenades[i].start();
 }
